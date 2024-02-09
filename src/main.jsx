7037f39ec390199c2e4ee3f8c4cbfbde7a47040c
Sachin Parihar <130788900+Sheenu-exe@users.noreply.github.com>
2024-02-09 12:52:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './assets/components/useAuth.jsx'; 
 import "./res.css"
+import "./App.css"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
